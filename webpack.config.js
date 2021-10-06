68c5f66d9e8ef70250892a81068844bbdd741c0b
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-10-06 12:23:22 +0300

    Moved server host to env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
@@ -29,6 +30,7 @@ module.exports = {
     },
     plugins: [
         new VueLoaderPlugin(),
+        new Dotenv()
     ],
     resolve: {
         extensions: ['.js', '.vue'],
