976dd08165c91e477df208a61cfafaa3722d812d
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-10-07 11:52:56 +0300

    Added validation using Vuelidate on login page

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -2,20 +2,41 @@
   <form @submit.prevent="login"
         class="position-absolute top-50 start-50 translate-middle w-50 p-5 border border-white rounded">
     <div class="input-data">
-      <label class="mt-3" for="username-input">Username</label>
-      <input id="username-input" name="username"
-             :class="{'login-input form-control': true, 'is-invalid': !isUsernameValid}"
-             type="text" autocomplete="off" v-model="username">
-      <label class="mt-3" for="password-input">Password</label>
-      <input id="password-input" name="password"
-             :class="{'login-input form-control': true, 'is-invalid': !isPasswordValid}"
-             type="password" v-model="password">
+      <div class="form-group">
+        <label class="mt-3" for="username-input">Username</label>
+        <input id="username-input" name="username"
+               :class="{'login-input form-control': true, 'is-invalid': v$.username.$error}"
+               type="text" autocomplete="off" v-model.trim="v$.username.$model">
+        <div class="invalid-feedback" v-if="v$.username.required.$invalid">Username is required.</div>
+        <div class="invalid-feedback" v-if="v$.username.minLengthValue.$invalid">Username must be longer than 3
+          characters.
+        </div>
+        <div class="invalid-feedback" v-if="v$.username.maxLengthValue.$invalid">Username must be shorter than 64
+          characters.
+        </div>
+        <div class="invalid-feedback" v-if="v$.username.username.$invalid">Username must contain only latin letters,
+          numbers and specific symbols like: "_", "."
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="mt-3" for="password-input">Password</label>
+        <input id="password-input" name="password"
+               :class="{'login-input form-control': true, 'is-invalid': v$.password.$error}"
+               type="password" v-model.trim="v$.password.$model">
+        <div class="invalid-feedback" v-if="v$.password.required.$invalid">Password is required.</div>
+        <div class="invalid-feedback" v-if="v$.password.minLengthValue.$invalid">Password must be longer than 8
+          characters.
+        </div>
+        <div class="invalid-feedback" v-if="v$.password.maxLengthValue.$invalid">Password must be shorter than 64
+          characters.
+        </div>
+      </div>
     </div>
     <div class="link-div mt-3 d-flex flex-column">
       <p>No account yet?
         <router-link to="/register">Register</router-link>
       </p>
-      <button class="btn btn-success w-50 align-self-center" :disabled="!isFormValid">Log in</button>
+      <button class="btn btn-success w-50 align-self-center" :disabled="v$.$invalid">Log in</button>
       <span class="text-danger align-self-center">{{ message }}</span>
     </div>
   </form>
@@ -23,32 +44,34 @@
 
 <script>
 import {login} from '/src/api/auth';
+import useVuelidate from '@vuelidate/core'
+import {required, minLength, maxLength} from '@vuelidate/validators'
 
 export default {
+  setup: () => {
+    return {v$: useVuelidate()}
+  },
   name: "Login",
   data: () => ({
     username: "",
     password: "",
-    isPasswordValid: true,
-    isUsernameValid: true,
     message: ""
   }),
-  watch: {
-    password(newValue) {
-      this.isPasswordValid = newValue.length >= 8;
-      this.message = this.isPasswordValid ? "" : "Password should be 8 characters or longer.";
+  validations: () => ({
+    username: {
+      required,
+      minLengthValue: minLength(3),
+      maxLengthValue: maxLength(64),
+      username: value => {
+        return value.match(/^[0-9a-zA-Z_.]*$/)
+      }
     },
-    username(newValue) {
-      this.isUsernameValid = newValue.match(/^[0-9a-zA-Z_.]{3,64}$/);
-      this.message = this.isUsernameValid ? "" : "Username should contain only latin letters, numbers and specific symbols \"_\", \".\" and have length from 3 to 64 characters.";
+    password: {
+      required,
+      minLengthValue: minLength(8),
+      maxLengthValue: maxLength(64)
     }
-  },
-  computed: {
-    isFormValid() {
-      return (this.isUsernameValid && !!this.username &&
-          this.isPasswordValid && !!this.password);
-    }
-  },
+  }),
   methods: {
     async login() {
       const data = JSON.stringify({
