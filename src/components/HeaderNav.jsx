1448c8f58a7a978eb8cad2a4dbd57c8bae484c22
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-08 16:57:34 +0200

    Created Login component so now it stores token in cookie

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,5 +1,9 @@
-export default function HeaderNav() {
+import {withCookies} from "react-cookie";
+
+export default withCookies(function HeaderNav(props) {
+    console.log("token", props.cookies.get("token"))
+    //temporary log for checking when cookies refresh
     return (
         <h1>HEADER</h1>
     )
-}
+});
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,85 @@
-export default function Login() {
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {login} from "../../api/auth";
+import {route} from "../../routes";
+import {Formik} from "formik";
+import {loginSchema} from "../../utils/validation/auth";
+import {Input} from "../form/Input";
+import {withCookies} from "react-cookie";
+
+export default withCookies(function Login(props) {
+
+    const initialData = {
+        username: "",
+        password: ""
+    };
+
+    const [message, setMessage] = useState("");
+
+    const navigate = useNavigate();
+
+    const submit = async (values) => {
+        try {
+            const response = await login(values);
+            props.cookies.set("token", response.token);
+            navigate(route("home"));
+        } catch (error) {
+            if (error.status === 401) {
+                setMessage("Incorrect username or password");
+            } else {
+                setMessage("Something went wrong");
+            }
+        }
+    }
+
     return (
-        <h1>Login</h1>
+        <Formik initialValues={initialData} onSubmit={submit} validationSchema={loginSchema}>
+            {({
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting
+              }) => (
+                <form onSubmit={handleSubmit}
+                      className="position-absolute top-50 start-50 translate-middle w-50 p-5 border border-white rounded">
+                    <div className="input-data">
+                        <Input
+                            field="username"
+                            errors={errors.username}
+                            touched={touched.username}
+                            events={{
+                                handleSubmit,
+                                handleBlur,
+                                handleChange
+                            }}
+                        />
+                        <Input
+                            field="password"
+                            errors={errors.password}
+                            touched={touched.password}
+                            events={{
+                                handleSubmit,
+                                handleBlur,
+                                handleChange
+                            }}
+                        />
+                    </div>
+                    <div className="link-div mt-3 d-flex flex-column">
+                        <p>
+                            No account yet?
+                            <Link to={route("register")}>Register</Link>
+                        </p>
+                        <button className="btn btn-success w-50 align-self-center" type={"submit"}
+                                disabled={isSubmitting}>Login
+                        </button>
+                        <span className="text-danger align-self-center">
+                                {message}
+                        </span>
+                    </div>
+                </form>
+            )}
+        </Formik>
     );
-}
+});
