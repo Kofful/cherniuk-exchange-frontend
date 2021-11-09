ec89c973b3a300273acf4d6d26fb1820a74b4716
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-09 10:56:29 +0200

    Added proptypes to HeaderNav component

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,11 +1,21 @@
-import {withCookies} from "react-cookie";
+import {Cookies, withCookies} from "react-cookie";
+import PropTypes from "prop-types";
 
-const HeaderNav = props => {
-    console.log("token", props.cookies.get("token"))
+const HeaderNav = ({cookies}) => {
+    console.log("token", cookies.get("token"))
     //temporary log for checking when cookies refresh
     return (
         <h1>HEADER</h1>
-    )
-}
+    );
+};
+
+
+HeaderNav.propTypes = {
+    cookies: PropTypes.instanceOf(Cookies)
+};
+
+HeaderNav.defaultProps = {
+    cookies: new Cookies()
+};
 
 export default withCookies(HeaderNav);
