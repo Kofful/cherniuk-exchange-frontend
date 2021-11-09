e3b9df54e6e9459f3d60e8ef2864c85b0bcdc8d7
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-09 11:13:31 +0200

    Added proptypes to Input component

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,18 +1,40 @@
-const Input = props => {
+import PropTypes from "prop-types";
+
+const Input = ({field, errors, touched, events}) => {
     return (
         <div className="form-group">
-            <label className="mt-3 text-capitalize" htmlFor={`${props.field}-input`}>{props.field}</label>
-            <input id={`${props.field}-input`} name={props.field}
-                   type={props.field === "password" ? "password" : "text"} autoComplete="off"
-                   className={`login-input form-control ${props.errors && props.touched ? "is-invalid" : ""}`}
-                   onChange={props.events.handleChange}
-                   onBlur={props.events.handleBlur}
+            <label className="mt-3 text-capitalize" htmlFor={`${field}-input`}>{field}</label>
+            <input id={`${field}-input`} name={field}
+                   type={field === "password" ? "password" : "text"} autoComplete="off"
+                   className={`login-input form-control ${errors && touched ? "is-invalid" : ""}`}
+                   onChange={events.handleChange}
+                   onBlur={events.handleBlur}
             />
-            {props.touched && props.errors && <div className="invalid-feedback">
-                {props.errors}
+            {touched && errors && <div className="invalid-feedback">
+                {errors}
             </div>}
         </div>
     );
-}
+};
+
+Input.propTypes = {
+    field: PropTypes.string,
+    errors: PropTypes.string,
+    touched: PropTypes.bool,
+    events: PropTypes.shape({
+        handleChange: PropTypes.func,
+        handleBlur: PropTypes.func
+    })
+};
+
+Input.defaultProps = {
+    field: "",
+    errors: "",
+    touched: false,
+    events: {
+        handleChange: ()=>{},
+        handleBlur: ()=>{}
+    }
+};
 
 export default Input;
