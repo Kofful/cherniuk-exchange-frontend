6701f36308cf72bc0ee69e5b49352fbaf95f546c
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-16 14:25:07 +0200

    Created spinner component

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import Loader from "react-loader-spinner";
+
+const Spinner = () => {
+    return (
+        <Loader type="BallTriangle"
+                color="#6600CC"
+                height={100}
+                width={100}/>
+    );
+};
+
+export default Spinner;
