7d3ce74db43d479289b93d5502300ac16f455736
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-18 11:49:15 +0200

    Added localization on error pages

--- src/components/errorPages/Page404.jsx
+++ src/components/errorPages/Page404.jsx
@@ -1,6 +1,13 @@
+import {FormattedMessage} from "react-intl";
+
 const Page404 = () => {
     return (
-        <h1>Page Not Found</h1>
+        <h1>
+            <FormattedMessage
+                id="page.not.found"
+                defaultMessage="Page Not Found"
+            />
+        </h1>
     );
 };
 
