3caf55458e4c0332c089c97dab23af9c358fc789
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2022-01-05 17:59:27 +0200

    Moved all logic connected with offer list from OfferListContainer to OfferFormContainer

--- src/components/offer/OfferListContainer.jsx
+++ src/components/offer/OfferListContainer.jsx
@@ -1,5 +1,4 @@
 import {useStore} from "../../stores";
-import {useEffect, useState} from "react";
 import Spinner from "../spinner/Spinner";
 import OfferContainer from "./OfferContainer";
 import PageButtons from "../pagination/PageButtons";
@@ -7,34 +6,14 @@ import {observer} from "mobx-react";
 import PropTypes from "prop-types";
 import {FormattedMessage} from "react-intl";
 
-const OfferListContainer = ({getOffers, isOpen}) => {
+const OfferListContainer = ({isOpen, isLoading, offerList, setOfferList, page, setPage, maxPages}) => {
     const {userStore} = useStore();
     const {user} = userStore;
-    const [page, setPage] = useState(1);
-    const [maxPages, setMaxPages] = useState(1);
-    const [isLoading, setIsLoading] = useState(false);
-    const [offerList, setOfferList] = useState([]);
-
-    const loadOffers = async () => {
-        setIsLoading(true);
-
-        const response = await getOffers(page);
-        const {offers, count} = response;
-        const maxPagesCount = Math.floor((count - 1) / process.env.REACT_APP_OFFERS_PER_PAGE + 1);
-
-        setMaxPages(maxPagesCount);
-        setIsLoading(false);
-        setOfferList(offers);
-    };
 
     const removeOffer = (offerId) => {
         const newOfferList = offerList.filter(item => item.id !== offerId);
         setOfferList(newOfferList);
     };
-
-    useEffect(() => {
-        loadOffers();
-    }, [page]);
 
     return (
         <div className="d-flex flex-column align-items-center">
@@ -69,13 +48,35 @@ const OfferListContainer = ({getOffers, isOpen}) => {
 };
 
 OfferListContainer.propTypes = {
-    getOffers: PropTypes.func,
-    isOpen: PropTypes.bool
+    isLoading: PropTypes.bool,
+    offerList: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            giveItems: PropTypes.array,
+            acceptItems: PropTypes.array,
+            creatorPayment: PropTypes.number,
+            targetPayment: PropTypes.number,
+            creator: PropTypes.shape({
+                id: PropTypes.number,
+                username: PropTypes.string
+            })
+        })
+    ),
+    setOfferList: PropTypes.func,
+    isOpen: PropTypes.bool,
+    page: PropTypes.number,
+    setPage: PropTypes.func,
+    maxPages: PropTypes.number,
 };
 
 OfferListContainer.defaultProps = {
-    getOffers: () => {},
-    isOpen: false
+    isLoading: false,
+    offerList: [],
+    setOfferList: () => {},
+    isOpen: false,
+    page: 1,
+    setPage: () => {},
+    maxPages: 1,
 };
 
 export default observer(OfferListContainer);
